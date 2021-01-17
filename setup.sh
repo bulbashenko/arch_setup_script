e29e15fa8c0440f349467daf1379125a959bf490
bulbashenko <bulbashenko69@gmail.com>
2021-01-17 16:23:04 +0500

    new files

--- setup.sh
+++ setup.sh
@@ -1,16 +1,48 @@
 #!/bin/sh
-#Install pikaur
-pacman -S --needed base-devel git
-git clone https://aur.archlinux.org/pikaur.git
-cd pikaur
-makepkg -fsri
 
-#Instal budgie
-pacman -S budgie-desktop gnome gdm arc-gtk-theme papirus-icon-theme
-useradd -m bulbashenko
-passwd bulbashenko
+echo "Username: "
+read NAME
+
+#Install sudo
+echo 'y' | pacman -S sudo
+useradd -m $NAME
+passwd $NAME
+echo "$NAME ALL=(ALL) ALL" | EDITOR='tee -a' visudo
+
+#Install budgie
+echo 'y' | pacman -S budgie-desktop gnome gdm
 systemctl enable gdm
-pikaur -S budgie-calendar-applet budgie-network-applet
+systemctl enable NetworkManager
+rm /etc/gdm/custom.conf
+cp custom.conf /etc/gdm/
 
-#Install apps
-pacman -S telegram-desktop discord firefox
+#Install pikaur
+su -s $NAME
+echo 'y' | sudo pacman -S --needed base-devel git
+cd pikaur
+makepkg -fsri < pikaur_setup.txt
+
+#Exit from user to root
+exit
+
+#Install applets && themes
+echo 'y' | pacman -S arc-gtk-theme papirus-icon-theme
+pikaur -S budgie-calendar-applet budgie-network-applet < applet_install.txt
+
+#Install font
+echo 'y' | pacman -S ttf-ubuntu-font-family
+
+#Install audio
+echo 'y' | pacman -S pulseaudio alsa
+cp daemon.conf ~/.config/pulse/
+
+#Install nvidia
+echo 'y' | pacman -S nvidia nvidia-settings bumblebee mesa-demos
+systemctl enable bumblebeed
+gpasswd -a $NAME bumblebee
+
+#Install soft
+echo 'y' | pacman -S firefox telegram-desktop discord
+
+#Restart system
+reboot
