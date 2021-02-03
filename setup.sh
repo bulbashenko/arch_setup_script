275c549b715ed302c2663657064084515ed84ec1
bulbashenko <bulbashenko69@gmail.com>
2021-02-03 11:17:14 +0000

    remove nvidia anduse pikaur

--- setup.sh
+++ setup.sh
@@ -3,12 +3,6 @@
 echo "Username: "
 read NAME
 
-#Add git config
-echo "github username: "
-read gitUser
-echo "github email: "
-read gitEmail
-
 #Install sudo && add user
 echo 'y' | pacman -S sudo
 groupadd $NAME
@@ -16,26 +10,7 @@ useradd -g $NAME -m $NAME
 passwd $NAME
 echo "$NAME ALL=(ALL) ALL" | EDITOR='tee -a' visudo
 
-
-#Install nvidia
-pacman -S nvidia nvidia-settings bumblebee
-systemctl enable bumblebeed
-gpasswd -a $NAME bumblebee
-
-#Install theme
-pacman -S arc-gtk-theme papirus-icon-theme
-
-#Install font
-pacman -S ttf-ubuntu-font-family
-
-#Install audio
-pacman -S pulseaudio alsa
-cp daemon.conf ~/.config/pulse/
-
-#Install apps
-pacman -S firefox telegram-desktop discord
-
-#Try to install pikaur
+#Install pikaur
 pacman -S --needed base-devel
 git clone https://aur.archlinux.org/pikaur.git
 cp -r pikaur/ /home/$NAME/
@@ -43,20 +18,22 @@ cd /home/$NAME/pikaur
 chown -R $NAME:$NAME /home/$NAME/pikaur/
 sudo -u $NAME makepkg -fsri
 
-#Install applets
-sudo -u $NAME pikaur -S budgie-network-applet
-systemctl enable gdm
+#Install audio
+pikaur -S pulseaudio
+cp daemon.conf /home/$NAME/.config/pulse/daemon.conf
 
 #Install budgie
-pikaut -S budgie-desktop-git gnome
+pikaur -S xorg xorg-server budgie-desktop-git gnome-terminal gdm
+echo 'y' | pikaur -S arc-gtk-theme papirus-icon-theme
+pikaur -S budgie-network-applet
 systemctl enable NetworkManager
+systemctl enable gdm
+
+#Install apps
+pikaur -S discord telegram-desktop firefox
 
 #Some features
 cd ~/arch_setup_script
-cp wallpaper.jpg /home/$NAME
-chown -R $NAME:$NAME /home/$NAME/wallpaper.jpg
-
-#Add git
-sudo -u $NAME git config --global user.name "$gitUser"
-sudo -u $NAME git config --global user.email "$gitEmail"
+cp -r Wallpapers/ /home/$NAME/Pictures
+chown -R $NAME:$NAME /home/$NAME/Pictures/Wallpapers
 
