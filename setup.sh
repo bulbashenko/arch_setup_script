fb8ca4d682b89d1a230cf80d12f333484687c188
bulbashenko <bulbashenko69@gmail.com>
2021-02-07 16:19:04 +0000

    add xdg nautilus eog and vlc

--- setup.sh
+++ setup.sh
@@ -20,12 +20,14 @@ sudo -u $NAME makepkg -fsri
 
 #Install audio
 pikaur -S pulseaudio
+mkdir /home/$NAME/.config
 mkdir /home/$NAME/.config/pulse
 cp daemon.conf /home/$NAME/.config/pulse
 
 #Install budgie
 pikaur -S xorg xorg-server budgie-desktop gnome-settings-daemon gnome-control-center gnome-screensaver lightdm lightdm-gtk-greeter
 pikaur -S gnome-terminal-transparency
+pikaur -S xdg-user-dirs
 pikaur -S plata-theme papirus-icon-theme
 pikaur -S budgie-network-applet budgie-screenshot-applet
 systemctl enable lightdm
@@ -34,12 +36,13 @@ systemctl enable lightdm
 pikaur -S nvidia nvidia-settings nvidia-prime
 
 #Install apps
-pikaur -S discord telegram-desktop firefox skypeforlinux-stable-bin mc
+pikaur -S discord telegram-desktop firefox skypeforlinux-stable-bin mc nautilus eog vlc
 
 #Some features
 cd ~/arch_setup_script
 mkdir /home/$NAME/Pictures
 cp -r Wallpapers/ /home/$NAME/Pictures
+chown -R $NAME:$NAME /home/$NAME/Pictures
 chown -R $NAME:$NAME /home/$NAME/Pictures/Wallpapers
 cp cooling /bin
 chown -R $NAME:$NAME /bin/cooling
